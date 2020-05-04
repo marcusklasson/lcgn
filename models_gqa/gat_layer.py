7830fd396558e018264029b826da2e471ddbc7d6
marcusklasson <mklas@kth.se>
2020-05-04 20:33:17 +0200

    added class for graph attention layer

---
+++ models_gqa/gat_layer.py
@@ -0,0 +1,21 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class GraphAttentionLayer(nn.Module):
+    """
+    Simple GAT layer, similar to https://arxiv.org/abs/1710.10903
+    """
+
+    def __init__(self, in_features, out_features, dropout, alpha, concat=True):
+        super(GraphAttentionLayer, self).__init__()
+        self.dropout = dropout
+        self.in_features = in_features
+        self.out_features = out_features
+        self.alpha = alpha
+        self.concat = concat
+
+    def forward(self, x):
+
+        pass
